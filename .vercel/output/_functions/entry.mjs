59f3e42e291d201580924856965c0ea43167adb6
cdryampi <cdryampi@gmail.com>
2025-02-07 09:47:12 +0100

    Update middleware secret and add new manifest for Vercel deployment

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_Dfb8y0tZ.mjs';
-import { manifest } from './manifest_CDs_LF84.mjs';
+import { manifest } from './manifest_BFlk92yh.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -20,7 +20,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "54cc8f7c-1efa-42b0-be51-ca0575772eca",
+    "middlewareSecret": "cdad2baa-9e83-4df2-aee9-14e2fe2cb207",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
